1781098da67cae2f49434a198e9de0b9c54ca51d
tboy276 <tuan.vuongdinh2706@gmail.com>
2026-08-30 07:07:15 +0700

    Auto-commit: Cleanup temporary script and zip files

--- replace-panel2.cjs
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/components/rfq/RealtimeSummaryPanel.tsx', 'utf8');
-
-content = content.replace(
-  /\{\(segment === 'forging' \|\| segment === 'casting'\) && \(/g,
-  "{(segment === 'forging' || segment === 'casting') && !isSeparateTooling && ("
-);
-
-fs.writeFileSync('src/components/rfq/RealtimeSummaryPanel.tsx', content, 'utf8');
--- replace-store.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/store/useQuotationStore.ts', 'utf8');
-
-content = content.replace(/C_pattern_total:\s*45000000/g, 'C_pattern_total: undefined');
-content = content.replace(/C_die_total:\s*85000000/g, 'C_die_total: undefined');
-
-fs.writeFileSync('src/store/useQuotationStore.ts', content, 'utf8');
